c17efaa0f6dac2117c5dc1d537c7f4f175b8a22b
Himanshu <hmcreationhimanshu012@gmail.com>
2026-04-14 15:31:10 +0530

    added a comment in the model class

--- FinanceGovApplication/src/main/java/com/financegov/model/DemoModel.java
+++ FinanceGovApplication/src/main/java/com/financegov/model/DemoModel.java
@@ -1,5 +1,5 @@
 package com.financegov.model;
 
 public class DemoModel {
-
+// hello from TL please do edit or delete these file it is mandatory to keep  this file as it is the basic structure for you 
 }
